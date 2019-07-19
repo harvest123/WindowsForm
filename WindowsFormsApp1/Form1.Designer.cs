34f599537d7a1b6edd31b286884fd2c8f597fb06
Jiang <jiang1cheng2hui@qq.com>
2019-07-19 13:10:57 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbox1 = new System.Windows.Forms.TextBox();
+            this.tbox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(413, 191);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "转换";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "输入6位十六进制数：";
+            // 
+            // tbox1
+            // 
+            this.tbox1.Location = new System.Drawing.Point(307, 191);
+            this.tbox1.Name = "tbox1";
+            this.tbox1.Size = new System.Drawing.Size(100, 21);
+            this.tbox1.TabIndex = 2;
+            // 
+            // tbox2
+            // 
+            this.tbox2.Location = new System.Drawing.Point(494, 191);
+            this.tbox2.Name = "tbox2";
+            this.tbox2.Size = new System.Drawing.Size(100, 21);
+            this.tbox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(747, 429);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "by半颗心";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbox2);
+            this.Controls.Add(this.tbox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbox1;
+        private System.Windows.Forms.TextBox tbox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            string input = tbox1.Text;
+            Regex regex = new Regex("^[0-9a-f[A-F]{6}$");
+            if (regex.IsMatch(input))
+            {
+                long[] number = new long[3];
+                for (int i = 0; i < 3; i++)
+                {
+                    string str = input.Substring(i * 2, 2);
+                    number[i] = Convert.ToInt32(str, 16);
+                }
+                number[0] >>= 3;
+                number[1] >>= 2;
+                number[2] >>= 3;
+                long output = number[2];
+                output += number[1] << 5;
+                output += number[0] << 11;
+                tbox2.Text = "";
+                tbox2.Text += string.Format("{0:X}", output);
+
+                //tbox2.Text = "num" + number[0];
+
+
+            }
+            else
+            {
+                MessageBox.Show("输入格式不正确", "ERROR");
+            }
+        }
+    }
+}
